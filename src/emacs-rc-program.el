523b60253414e5494d509d68ce6ea9e68c1d916d
Simon.Wu <simon.wu@c3-e.com>
2011-09-20 15:18:30 +0800

    rename ext to program related config

---
+++ src/emacs-rc-program.el
@@ -0,0 +1,25 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; haskell mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; JS2-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; JS-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;(add-hook 'js-mode-hook
+;          (lambda ()
+;            ;; Scan the file for nested code blocks
+;            (imenu-add-menubar-index)
+;            ;; Activate the folding mode
+;            (hs-minor-mode t)))
+
+(require 'flymake-cursor)             ;;; TODO: more about flymake??
