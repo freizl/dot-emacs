a8074f57e1efb883a2ec8c08fa7e507c62102063
Simon.Wu <simon.wu@c3-e.com>
2011-09-21 11:25:38 +0800

    add hs-minor to js2-mode

--- src/emacs-rc-program.el
+++ src/emacs-rc-program.el
@@ -11,6 +11,8 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-hook 'js2-mode-hook (lambda ()
+                           (hs-minor-mode t)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; JS-mode
