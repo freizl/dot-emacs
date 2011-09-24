34489da7cde36623cc27dad3734986d3f8989656
haisheng.wu <freizl@gmail.com>
2011-09-24 08:11:59 +0800

    add required for haskell

--- src/emacs-rc-program.el
+++ src/emacs-rc-program.el
@@ -1,6 +1,8 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; haskell mode
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'haskell-mode)
+(require 'inf-haskell)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
